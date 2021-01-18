ac9a59a6ddf18afdded8dd26bb85514d7b065cb6
RohitMidha23 <rohit.midha23@gmail.com>
2021-01-18 23:07:49 +0530

    changed to timm

--- Model.py
+++ Model.py
@@ -1,23 +1,22 @@
 import torch
 import torchvision
+import timm
+
 import numpy as np
+
 from torch import nn, optim
 
 
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.input_size = 784
-        self.hidden_sizes = [128, 64]
-        self.output_size = 10
-        self.conv = nn.Sequential(nn.Linear(self.input_size, self.hidden_sizes[0]),
-                                  nn.ReLU(),
-                                  nn.Linear(self.hidden_sizes[0], self.hidden_sizes[1]),
-                                  nn.ReLU(),
-                                  nn.Linear(self.hidden_sizes[1], self.output_size),
-                                  nn.LogSoftmax(dim=1))
 
-    def forward(self, x):
-        return self.conv(x)
+
+class Model(nn.Module): 
+    def __init__(self, model_name="resnext101_32x8d", n_classes=6, pretrained=True):
+        super().__init__()
+        self.model = timm.create_model(model_name, pretrained=pretrained)
+        input_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(input_features, n_classes)
+    def forward(self, x): 
+        x = self.model(x)
+        return x
 
     
