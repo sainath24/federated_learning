ece96cfaeafa140bf2e6d547de79399173aeaaa4
RohitMidha23 <rohit.midha23@gmail.com>
2021-01-19 21:17:56 +0530

    model py file

---
+++ client/model.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+import timm
+
+import numpy as np
+
+from torch import nn
+
+class Model(nn.Module): 
+    def __init__(self, arch="resnext101_32x8d", n_classes=6, pretrained=True):
+        super().__init__()
+        self.model = timm.create_model(arch, pretrained=pretrained)
+        input_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(input_features, n_classes)
+    def forward(self, x): 
+        x = self.model(x)
+        return x
+
+    
