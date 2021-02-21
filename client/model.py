86c273867623c3ef0793e030edc930eab8a7b73a
RohitMidha23 <rohit.midha23@gmail.com>
2021-02-21 18:09:47 +0530

    fix

--- client/model.py
+++ client/model.py
@@ -18,18 +18,20 @@ class ClassificationModel(nn.Module):
         x = self.model(x)
         return x
 
+
 class DetectionModel(nn.Module):
-    def __init__(self, arch="fasterrcnn_resnet50_fpn", n_classes=6, pretrained=True):
-            super().__init__()
-            self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-                pretrained=pretrained, 
-                min_size=1024
-            )
-            input_features = model.roi_heads.box_predictor.cls_score.in_features
-            model.roi_heads.box_predictor = FastRCNNPredictor(
-                input_features, n_classes
-            )
-    def forward(self, x): 
+    def __init__(self, arch="fasterrcnn_resnet50_fpn", n_classes=2, pretrained=True):
+        super().__init__()
+        self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+            pretrained=pretrained,
+            min_size=1024
+        )
+        input_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(
+            input_features, n_classes
+        )
+
+    def forward(self, x):
         x = self.model(x)
         return x
     
--- server/model.py
+++ server/model.py
@@ -21,14 +21,14 @@ class ClassificationModel(nn.Module):
 
 
 class DetectionModel(nn.Module):
-    def __init__(self, arch="fasterrcnn_resnet50_fpn", n_classes=6, pretrained=True):
+    def __init__(self, arch="fasterrcnn_resnet50_fpn", n_classes=2, pretrained=True):
         super().__init__()
         self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
             pretrained=pretrained,
             min_size=1024
         )
-        input_features = model.roi_heads.box_predictor.cls_score.in_features
-        model.roi_heads.box_predictor = FastRCNNPredictor(
+        input_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(
             input_features, n_classes
         )
 
